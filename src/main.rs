3f1e63e96a9a46a5a47f3e899241f31691a4fd26
Andreas Hartel <andreas.hartel@aleph-alpha.com>
2022-10-04 16:10:11 +0200

    can serialize 2d positions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Position {
     x: u32,
     y: u32,
@@ -17,10 +17,11 @@ impl Position {
 /// Returns allowed moves from a given position
 pub struct AllowedPositions {
     pub allowed_positions: Vec<Position>,
+    index: usize,
 }
 
 impl AllowedPositions {
-    pub fn new(current_position: Position) -> AllowedPositions {
+    pub fn new(current_position: Position, board_size: u32) -> AllowedPositions {
         let possible_moves: Vec<(i32, i32)> = vec![
             (-2, -1),
             (2, -1),
@@ -39,34 +40,44 @@ impl AllowedPositions {
                 (new_x, new_y)
             })
             .filter(|&(x, y)| x >= 0 && y >= 0)
+            .map(|(x, y)| (x as u32, y as u32))
+            .filter(|&(x, y)| x < board_size && y < board_size)
             .map(|(x, y)| Position::new(x as u32, y as u32))
             .collect();
-        AllowedPositions { allowed_positions }
+        AllowedPositions {
+            allowed_positions,
+            index: 0,
+        }
+    }
+}
+
+impl Iterator for AllowedPositions {
+    type Item = Position;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.allowed_positions.get(self.index);
+        if result.is_some() {
+            self.index += 1;
+        }
+        result.copied()
     }
 }
 
 pub struct VisitedPositions {
-    visited_positions: Vec<u32>,
-    board_size: u32,
+    visited_positions: Vec<Position>,
 }
 
 impl VisitedPositions {
-    pub fn new(positions: Vec<u32>, board_size: u32) -> VisitedPositions {
+    pub fn new(positions: Vec<Position>) -> VisitedPositions {
         VisitedPositions {
             visited_positions: positions,
-            board_size,
         }
     }
 
     pub fn serialize(&self) -> String {
         self.visited_positions
             .iter()
-            .map(|pos| {
-                char::from_u32(pos % self.board_size + 97)
-                    .unwrap()
-                    .to_string()
-                    + &(pos / self.board_size + 1).to_string()
-            })
+            .map(|pos| char::from_u32(pos.x + 97).unwrap().to_string() + &(pos.y + 1).to_string())
             .collect::<Vec<String>>()
             .join(" ")
     }
@@ -83,32 +94,38 @@ mod tests {
     }
     #[test]
     fn should_return_a1() {
-        let moves = VisitedPositions::new(vec![0], 5);
+        let moves = VisitedPositions::new(vec![Position::new(0, 0)]);
         assert_eq!(moves.serialize(), "a1")
     }
     #[test]
     fn should_return_a1b1() {
-        let moves = VisitedPositions::new(vec![0, 1], 5);
+        let moves = VisitedPositions::new(vec![Position::new(0, 0), Position::new(1, 0)]);
         assert_eq!(moves.serialize(), "a1 b1")
     }
     #[test]
     fn should_return_a1a2() {
-        let moves = VisitedPositions::new(vec![0, 5], 5);
+        let moves = VisitedPositions::new(vec![Position::new(0, 0), Position::new(0, 1)]);
         assert_eq!(moves.serialize(), "a1 a2")
     }
 
     #[test]
     fn eight_allowed_positions_from_center() {
         let current = Position::new(2, 2);
-        let allowed_positions = AllowedPositions::new(current);
-        assert_eq!(allowed_positions.allowed_positions.len(), 8);
+        let allowed_positions = AllowedPositions::new(current, 5);
+        assert_eq!(allowed_positions.count(), 8);
     }
 
     #[test]
     fn two_allowed_positions_from_top_left() {
         let current = Position::new(0, 0);
-        let allowed_positions = AllowedPositions::new(current);
-        dbg!(&allowed_positions.allowed_positions);
-        assert_eq!(allowed_positions.allowed_positions.len(), 2);
+        let allowed_positions = AllowedPositions::new(current, 5);
+        assert_eq!(allowed_positions.count(), 2);
+    }
+
+    #[test]
+    fn two_allowed_positions_from_top_right() {
+        let current = Position::new(4, 0);
+        let allowed_positions = AllowedPositions::new(current, 5);
+        assert_eq!(allowed_positions.count(), 2);
     }
 }
