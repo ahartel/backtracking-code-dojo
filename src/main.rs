d8dbce53277e45c31c166515499b842f60dc2a84
Andreas Hartel <andreas.hartel@aleph-alpha.com>
2022-10-04 15:52:13 +0200

    can find allowed moves

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,64 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub struct Moves {
-    moves: Vec<u32>,
+#[derive(Debug)]
+pub struct Position {
+    x: u32,
+    y: u32,
+}
+
+impl Position {
+    pub fn new(x: u32, y: u32) -> Position {
+        Position { x, y }
+    }
+}
+
+/// Returns allowed moves from a given position
+pub struct AllowedPositions {
+    pub allowed_positions: Vec<Position>,
+}
+
+impl AllowedPositions {
+    pub fn new(current_position: Position) -> AllowedPositions {
+        let possible_moves: Vec<(i32, i32)> = vec![
+            (-2, -1),
+            (2, -1),
+            (1, -2),
+            (-1, -2),
+            (-2, 1),
+            (2, 1),
+            (-1, 2),
+            (1, 2),
+        ];
+        let allowed_positions = possible_moves
+            .into_iter()
+            .map(|(x, y)| {
+                let new_x = current_position.x as i32 + x;
+                let new_y = current_position.y as i32 + y;
+                (new_x, new_y)
+            })
+            .filter(|&(x, y)| x >= 0 && y >= 0)
+            .map(|(x, y)| Position::new(x as u32, y as u32))
+            .collect();
+        AllowedPositions { allowed_positions }
+    }
+}
+
+pub struct VisitedPositions {
+    visited_positions: Vec<u32>,
     board_size: u32,
 }
 
-impl Moves {
-    pub fn new(moves: Vec<u32>, board_size: u32) -> Moves {
-        Moves { moves, board_size }
+impl VisitedPositions {
+    pub fn new(positions: Vec<u32>, board_size: u32) -> VisitedPositions {
+        VisitedPositions {
+            visited_positions: positions,
+            board_size,
+        }
     }
 
     pub fn serialize(&self) -> String {
-        self.moves
+        self.visited_positions
             .iter()
             .map(|pos| {
                 char::from_u32(pos % self.board_size + 97)
@@ -28,7 +74,7 @@ impl Moves {
 
 #[cfg(test)]
 mod tests {
-    use crate::Moves;
+    use crate::{AllowedPositions, Position, VisitedPositions};
 
     #[test]
     fn it_works() {
@@ -37,17 +83,32 @@ mod tests {
     }
     #[test]
     fn should_return_a1() {
-        let moves = Moves::new(vec![0], 5);
+        let moves = VisitedPositions::new(vec![0], 5);
         assert_eq!(moves.serialize(), "a1")
     }
     #[test]
     fn should_return_a1b1() {
-        let moves = Moves::new(vec![0, 1], 5);
+        let moves = VisitedPositions::new(vec![0, 1], 5);
         assert_eq!(moves.serialize(), "a1 b1")
     }
     #[test]
     fn should_return_a1a2() {
-        let moves = Moves::new(vec![0, 5], 5);
+        let moves = VisitedPositions::new(vec![0, 5], 5);
         assert_eq!(moves.serialize(), "a1 a2")
+    }
+
+    #[test]
+    fn eight_allowed_positions_from_center() {
+        let current = Position::new(2, 2);
+        let allowed_positions = AllowedPositions::new(current);
+        assert_eq!(allowed_positions.allowed_positions.len(), 8);
+    }
+
+    #[test]
+    fn two_allowed_positions_from_top_left() {
+        let current = Position::new(0, 0);
+        let allowed_positions = AllowedPositions::new(current);
+        dbg!(&allowed_positions.allowed_positions);
+        assert_eq!(allowed_positions.allowed_positions.len(), 2);
     }
 }
